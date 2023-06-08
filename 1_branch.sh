4891949e0d97f1a1b2d39bc1acbe5dc06bf93e65
88888812 <ndp2012@yandex.ru>
2023-06-08 12:41:44 +0300

    added rebase sh

--- 1_branch.sh
+++ 1_branch.sh
@@ -1,4 +1,12 @@
 # write file using cat
 git branch 1
 git checkout 1
-# git checkout -b 1 are equavalent cmd
+# git checkout -b 1 are equivalent cmd
+
+git add 1_branch.sh
+git commit -m ''
+# also had a commit from master and want to have it on our branch
+git rebase master
+# after that merge with master using git flow technology
+git checkout master
+git merge 1
