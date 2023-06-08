36af778e604147ec07f1207454c8d0a0f3625212
88888812 <ndp2012@yandex.ru>
2023-06-08 12:44:35 +0300

    trunk based

--- 1_branch.sh
+++ 1_branch.sh
@@ -7,6 +7,6 @@ git add 1_branch.sh
 git commit -m ''
 # also had a commit from master and want to have it on our branch
 git rebase master
-# after that merge with master using git flow technology
+# after that merge with master using git trunk based technology
 git checkout master
 git merge 1
