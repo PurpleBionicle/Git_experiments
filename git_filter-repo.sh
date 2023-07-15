30269d0d1fd6ca9f6f13a60ed3ba5b09e280231e
88888812 <ndp2012@yandex.ru>
2023-07-15 20:06:32 +0300

    remove sensitive data

--- git_filter-repo.sh
+++ git_filter-repo.sh
@@ -1,1 +1,7 @@
 echo my password is ***REMOVED***
+# after use a filter-repo
+apt install git-filter-repo
+git filter-repo --replace-text password.txt --force
+
+# but my local repo lose a remote
+# so i must linked it again
